cc8af7b500dd23d2941f2fc100a2ad5260c74d69
timnoack <noack@esa.tu-darmstadt.de>
2024-01-08 15:06:42 +0100

    Initial commit

---
+++ src/matrixExporter.C
@@ -0,0 +1,153 @@
+#include <fstream>
+
+#include "fast_matrix_market/app/doublet.hpp"
+#include "fast_matrix_market/fast_matrix_market.hpp"
+#include "fast_matrix_market/types.hpp"
+#include "matrixExporter.H"
+
+namespace Foam {
+defineTypeNameAndDebug(matrixExporter, 0);
+
+lduMatrix::solver::addsymMatrixConstructorToTable<matrixExporter>
+    addsmoothSolverSymMatrixConstructorToTable_;
+
+lduMatrix::solver::addasymMatrixConstructorToTable<matrixExporter>
+    addsmoothSolverAsymMatrixConstructorToTable_;
+}  // namespace Foam
+
+Foam::matrixExporter::matrixExporter(
+    const word &fieldName, const lduMatrix &matrix,
+    const FieldField<Field, scalar> &interfaceBouCoeffs,
+    const FieldField<Field, scalar> &interfaceIntCoeffs,
+    const lduInterfaceFieldPtrsList &interfaces,
+    const dictionary &solverControls)
+    : lduMatrix::solver(fieldName, matrix, interfaceBouCoeffs,
+                        interfaceIntCoeffs, interfaces, solverControls) {
+  readControls();
+}
+
+void Foam::matrixExporter::readControls() {
+  lduMatrix::solver::readControls();
+  fileNameA_ = controlDict_.getOrDefault<fileName>("matrixFile", "A.mtx");
+  fileNameb_ = controlDict_.getOrDefault<fileName>("sourceFile", "b.mtx");
+  fileNamex_ = controlDict_.getOrDefault<fileName>("initialFieldFile", "x.mtx");
+  comment_ =
+      controlDict_.getOrDefault<string>("comment", "No description provided");
+}
+
+void Foam::matrixExporter::exportMatrix() const {
+  Info << "Exporting matrix to file " << fileNameA_ << endl;
+
+  label numRows = matrix_.lduAddr().size();
+  label numFaces = matrix_.lduAddr().lowerAddr().size();
+  label numNonZeros = numRows + 2 * numFaces;
+
+  fast_matrix_market::matrix_market_header header;
+  header.comment = comment_;
+  header.nrows = numRows;
+  header.ncols = numRows;
+  header.format = fast_matrix_market::coordinate;
+  header.field = fast_matrix_market::real;
+  header.symmetry = matrix_.symmetric() ? fast_matrix_market::symmetric
+                                        : fast_matrix_market::general;
+
+  std::vector<label> rows;
+  std::vector<label> cols;
+  std::vector<scalar> values;
+
+  rows.reserve(numNonZeros);
+  cols.reserve(numNonZeros);
+  values.reserve(numNonZeros);
+
+  auto addCoeffs = [&](const label row, const label col, const scalar coeff) {
+    rows.push_back(row);
+    cols.push_back(col);
+    values.push_back(coeff);
+  };
+
+  forAll(matrix_.lduAddr(), row) {
+    // Add diagonal coefficient
+    addCoeffs(row, row, matrix_.diag()[row]);
+
+    // Add lower coefficients of this row
+    label ownerStart = matrix_.lduAddr().ownerStartAddr()[row];
+    label ownerEnd = matrix_.lduAddr().ownerStartAddr()[row + 1];
+    for (label facei = ownerStart; facei < ownerEnd; ++facei) {
+      label neighbourCell = matrix_.lduAddr().upperAddr()[facei];
+      scalar coeff = matrix_.lower()[facei];
+      addCoeffs(row, neighbourCell, coeff);
+    }
+
+    // Add upper coefficients of this row
+    label losortStart = matrix_.lduAddr().losortStartAddr()[row];
+    label losortEnd = matrix_.lduAddr().losortStartAddr()[row + 1];
+    for (label i = losortStart; i < losortEnd; ++i) {
+      label facei = matrix_.lduAddr().losortAddr()[i];
+      label ownerCell = matrix_.lduAddr().lowerAddr()[facei];
+      scalar coeff = matrix_.upper()[facei];
+      addCoeffs(row, ownerCell, coeff);
+    }
+  }
+
+  std::ofstream os(fileNameA_.c_str());
+
+  if (!os) {
+    FatalErrorIn("matrixExporter::exportMatrix()")
+        << "Cannot open file " << fileNameA_ << endl;
+  }
+
+  fast_matrix_market::write_matrix_market_triplet(os, header, rows, cols,
+                                                  values);
+  os.close();
+}
+
+void Foam::matrixExporter::exportField(const scalarField &field,
+                                       fileName fileName) const {
+  Info << "Exporting field to file " << fileName << endl;
+  label numRows = matrix_.lduAddr().size();
+
+  fast_matrix_market::matrix_market_header header;
+  header.comment = comment_;
+  header.nrows = numRows;
+  header.ncols = 1;
+  header.format = fast_matrix_market::coordinate;
+  header.field = fast_matrix_market::real;
+  header.symmetry = fast_matrix_market::general;
+
+  std::vector<label> rows;
+  std::vector<scalar> values;
+
+  rows.reserve(numRows);
+  values.reserve(numRows);
+
+  forAll(field, row) {
+    rows.push_back(row);
+    values.push_back(field[row]);
+  }
+
+  std::ofstream os(fileName.c_str());
+
+  if (!os) {
+    FatalErrorIn("matrixExporter::field()")
+        << "Cannot open file " << fileName << endl;
+  }
+
+  fast_matrix_market::write_matrix_market_doublet(os, header, rows, values);
+  os.close();
+}
+
+Foam::solverPerformance Foam::matrixExporter::solve(
+    scalarField &psi_s, const scalarField &source, const direction cmpt) const {
+  solverPerformance solverPerf(typeName, fieldName_);
+
+  Info << "Export-solver invoked for field " << fieldName_ << endl;
+
+  // Export matrix, source and initial field
+  exportMatrix();
+  exportField(source, fileNameb_);
+  exportField(psi_s, fileNamex_);
+
+  Info << "Export-solver finished for field " << fieldName_ << endl;
+
+  return solverPerf;
+}
